0e2a4965aec6f12f1b37dde6609e90455913b9db
Jazulienux <jazulienux@gmail.com>
2019-10-13 23:38:21 +0700

    Buttom Navigation

--- app/src/main/java/com/example/qliniue/activities/Login.java
+++ app/src/main/java/com/example/qliniue/activities/Login.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.qliniue.R;
 
@@ -19,5 +21,10 @@ public class Login extends AppCompatActivity {
     public void handleRegistration(View view) {
         Intent i = new Intent(Login.this, Registration.class);
         startActivity(i);
+    }
+
+    public void handleApp(View view){
+            Intent i = new Intent(Login.this, MainActivity.class);
+            startActivity(i);
     }
 }
--- app/src/main/java/com/example/qliniue/activities/MainActivity.java
+++ app/src/main/java/com/example/qliniue/activities/MainActivity.java
@@ -1,18 +1,72 @@
 package com.example.qliniue.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 
 import com.example.qliniue.R;
+import com.example.qliniue.fragments.AntrianFragment;
+import com.example.qliniue.fragments.HomeFragment;
+import com.example.qliniue.fragments.KonsulFragment;
+import com.example.qliniue.fragments.MedicalRecordFragment;
+import com.example.qliniue.fragments.PengaturanFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.halaman_utama:
+                fragment = new HomeFragment();
+                break;
+            case R.id.antrian:
+                fragment = new AntrianFragment();
+                break;
+            case R.id.medical_record:
+                fragment = new MedicalRecordFragment();
+                break;
+
+            case R.id.konsultasi:
+                fragment = new KonsulFragment();
+                break;
+
+            case R.id.pengaturan:
+                fragment = new PengaturanFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
